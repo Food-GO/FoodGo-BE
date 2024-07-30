c5aa29873dd5458da5c5052d4d19005f222fcac6
xxoznge <20201049@sungshin.ac.kr>
2024-07-30 11:10:15 +0900

    refactor: SwaggerConfig 수정

--- common-module/src/main/java/com/foodgo/commonmodule/config/SwaggerConfig.java
+++ common-module/src/main/java/com/foodgo/commonmodule/config/SwaggerConfig.java
@@ -1,28 +1,48 @@
 package com.foodgo.commonmodule.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(
-                title = "HANEUM FOODGO API 명세서",
-                description = "HANEUM FOODGO BE API 명세서입니다.",
-                version = "v1")
-)
-@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
 
+    // url: http://localhost:8080/swagger-ui/index.html#/
     @Bean
-    public GroupedOpenApi stOpenApi() {
+    public OpenAPI getOpenApi() {
+        Server server = new Server().url("/");
 
-        return GroupedOpenApi.builder()
-                .group("HANEUM FOODGO API v1")
-                .pathsToMatch("/**")
-                .build();
+        return new OpenAPI()
+            .info(getSwaggerInfo())
+            .components(authSetting())
+            .addServersItem(server)
+            .addSecurityItem(new SecurityRequirement().addList("access-token"));
+    }
+
+    private Info getSwaggerInfo() {
+        License license = new License();
+        license.setName("FOODGO");
+
+        return new Info()
+            .title("FOODGO API Document")
+            .description("This is FOODGO's API document.")
+            .version("v0.0.1")
+            .license(license);
+    }
+
+    private Components authSetting() {
+        return new Components()
+            .addSecuritySchemes(
+                "access-token",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT"));
     }
 }
-
