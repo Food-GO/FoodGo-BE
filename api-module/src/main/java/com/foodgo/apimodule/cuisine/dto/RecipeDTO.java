2f78e9f418f4bcaba67b3e931d8729a15c9ae9de
안정후 <austinan@naver.com>
2024-08-10 16:02:27 +0900

    fix: image 제거

--- api-module/src/main/java/com/foodgo/apimodule/cuisine/dto/RecipeDTO.java
+++ api-module/src/main/java/com/foodgo/apimodule/cuisine/dto/RecipeDTO.java
@@ -6,17 +6,19 @@ public class RecipeDTO {
 
     public record Request(
             String rcpNm  // 메뉴명
-    ) {}
+    ) {
+    }
 
     // 전체 응답을 나타내는 클래스
-    public record TotalResponse (
+    public record TotalResponse(
             CookRcp COOKRCP01
     ) {
         public record CookRcp(
                 String total_count,
                 List<RecipeDetail> row,  // 여러 개의 레시피 데이터를 포함하는 리스트
                 Result RESULT
-        ) {}
+        ) {
+        }
     }
 
     // 개별 레시피 데이터를 나타내는 클래스
@@ -55,32 +57,14 @@ public class RecipeDTO {
             String MANUAL18,             // 조리법 18
             String MANUAL19,             // 조리법 19
             String MANUAL20,             // 조리법 20
-            String MANUAL_IMG01,         // 조리법 이미지 1
-            String MANUAL_IMG02,         // 조리법 이미지 2
-            String MANUAL_IMG03,         // 조리법 이미지 3
-            String MANUAL_IMG04,         // 조리법 이미지 4
-            String MANUAL_IMG05,         // 조리법 이미지 5
-            String MANUAL_IMG06,         // 조리법 이미지 6
-            String MANUAL_IMG07,         // 조리법 이미지 7
-            String MANUAL_IMG08,         // 조리법 이미지 8
-            String MANUAL_IMG09,         // 조리법 이미지 9
-            String MANUAL_IMG10,         // 조리법 이미지 10
-            String MANUAL_IMG11,         // 조리법 이미지 11
-            String MANUAL_IMG12,         // 조리법 이미지 12
-            String MANUAL_IMG13,         // 조리법 이미지 13
-            String MANUAL_IMG14,         // 조리법 이미지 14
-            String MANUAL_IMG15,         // 조리법 이미지 15
-            String MANUAL_IMG16,         // 조리법 이미지 16
-            String MANUAL_IMG17,         // 조리법 이미지 17
-            String MANUAL_IMG18,         // 조리법 이미지 18
-            String MANUAL_IMG19,         // 조리법 이미지 19
-            String MANUAL_IMG20,         // 조리법 이미지 20
             String RCP_NA_TIP            // 저감 조리법 TIP
-    ) {}
+    ) {
+    }
 
     // 결과 메시지를 나타내는 클래스
     public record Result(
             String MSG,
             String CODE
-    ) {}
+    ) {
+    }
 }
